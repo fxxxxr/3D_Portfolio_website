0c0e441c05432419f9be8397e25e8f4f1475128a
DEEPANSHU SINGH GAUTAM <ds5561674@gmail.com>
2024-12-26 20:27:17 +0530

    minute changes in logo and orientation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,8 +44,8 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Adrian &nbsp;
-            <span className='sm:block hidden'> | JavaScript Mastery</span>
+            Deepanshu &nbsp;
+            <span className='sm:block hidden'> | Blockchain Developer</span>
           </p>
         </Link>
 
@@ -67,7 +67,7 @@ const Navbar = () => {
           <img
             src={toggle ? close : menu}
             alt='menu'
-            className='w-[28px] h-[28px] object-contain'
+            className='w-[28px] h-[28px] object-contain cursor-pointer'
             onClick={() => setToggle(!toggle)}
           />
 
