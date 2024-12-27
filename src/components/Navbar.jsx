75fc39d47a854353e775d1f81ba84aaf179191d4
DEEPANSHU SINGH GAUTAM <ds5561674@gmail.com>
2024-12-27 20:13:16 +0530

    minute code debugging

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { styles } from "../styles";
 import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
+console.log(menu, close);
 
 const Navbar = () => {
   const [active, setActive] = useState("");
